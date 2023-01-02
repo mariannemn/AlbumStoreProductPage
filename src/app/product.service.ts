7d8146d1f6a8b0349fd949165a4828274c5e5a2c
Marianne Mongin <marianne.mongin@capgemini.com>
2023-01-02 15:19:38 +0100

    service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private _albumUrl = '../assets/album.json';
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).pipe(map((response: any) => response.json()));
+  }
 }
