bf15aff85c8007b59e59cb8dc35019cdc40d08d2
Marianne Mongin <marianne.mongin@capgemini.com>
2023-01-04 14:59:42 +0100

    list of products component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,17 +3,24 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Album } from './album';
 import { Observable } from 'rxjs/internal/Observable';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: HttpClient) { }
 
   getAlbum(_id: number): Observable<Album> {
     return this._http.get(this._albumUrl).pipe(map(response =>
-       <Album> response));
+       <Album>response));
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).pipe(map(response =>
+      <Product[]>response))
   }
 }
